0a7b77331c2c6be7b759faa2ea462c65cd63c0b9
ElainaHalls <130600410+ElainaHalls@users.noreply.github.com>
2024-03-08 08:17:28 -0700

    Added the list of teams with their additional info

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import teamData from './CollegeBasketballTeams.json'; // Import the JSON data
 
+interface Team {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Heading() {
+  return <h1>March Madness - NCAA Basketball Teams</h1>;
+}
+
+// Team card component
+function TeamCard({ team }: { team: Team }) {
+  const { school, name, city, state } = team;
+
+  return (
+    <div className="team-card">
+      <h2>{school}</h2>
+      <p>Mascot: {name}</p>
+      <p>
+        {' '}
+        Location : {city}, {state}
+      </p>
+    </div>
+  );
+}
+
+// List of team cards component
+function TeamList() {
+  return (
+    <div className="team-list">
+      {teamData.teams.map((team, index) => (
+        <TeamCard key={index} team={team} />
+      ))}
+    </div>
+  );
+}
+
+// App component
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <TeamList />
     </div>
   );
 }
